6cbeccfecf7de8290fbf800ae38f886a83c4d2b7
Jameson Taylor <jameson@ionic.io>
2024-03-06 15:21:24 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'multistep-flow-routing',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
